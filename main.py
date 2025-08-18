258aa4ac0702a201ca193fa733a691b6afcf994e
Anyim Ossi <ossifavour@gmail.com>
2025-08-18 07:40:12 +0100

    include default route

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ async def startup_event():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+@app.get("/")
+def index():
+    return {"message": "Welcome to XenToba Gateway & User Management System",
+            "next": "'#/docs' for documentation"}
+
 @app.get("/health")
 async def health_check():
     return {"status": "healthy"}
